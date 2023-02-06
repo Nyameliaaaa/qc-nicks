4d60e5a1c6bdc2cc394abf141e0157b4ec9f9b36
Amelia Katherine <32780515+night-lake@users.noreply.github.com>
2023-02-06 17:52:47 +0300

    shush

--- components/GradientModal.vue
+++ components/GradientModal.vue
@@ -76,7 +76,7 @@ const updateColor = (event: ColorChangeEvent) => {
 };
 </script>
 
-<style>
+<style lang="">
 #color-picker-color-hex {
 	@apply mt-0 block w-full border-0 border-b-2 border-black bg-white px-0.5 transition-all duration-500 focus:border-pink-600 focus:ring-0 dark:border-gray-200 dark:bg-neutral-800 dark:text-white dark:focus:border-pink-500;
 }
--- components/NicknamePreview.vue
+++ components/NicknamePreview.vue
@@ -1,11 +1,9 @@
 <template>
 	<div class="sticky top-0 z-50 shadow-lg">
 		<block class="flex flex-col gap-2">
-			<h2 class="text-md font-medium text-gray-800 dark:text-gray-400">
-				Preview
-			</h2>
+			<h2 class="text-md font-medium text-gray-800 dark:text-gray-400">Preview</h2>
 			<p
-				class="rounded-md py-2 text-center text-3xl font-bold"
+				class="rounded-md py-2 text-center text-3xl font-bold text-white"
 				:class="`[background-image:_linear-gradient(#131a1b,#131a1b),_url('/assets/img/blank_sign.png')] [background-blend-mode:_hard-light]`"
 			>
 				<span
