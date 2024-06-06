96623e5a4370fbf0b9508be5471e3bb318530a41
Amelia Rose <nyameliaaaa@proton.me>
2024-06-06 21:17:30 +0300

    fix nick reset fr this time

--- composables/handlers/useUpdateGradient.ts
+++ composables/handlers/useUpdateGradient.ts
@@ -5,26 +5,26 @@ export default function () {
 	if (nick.value.length < colors.value.length) {
 		useUpdateModal(true, 'GradientSize', { colorLen: colors.value.length, nickLen: nick.value.length });
 		return;
+	} else {
+		let out = "";
+		nick.value = nick.value.replace(/&([0-9a-f]|#[0-9a-f]{6})/gi, "");
+	
+		const sections = nick.value.length / (colors.value.length - 1);
+		const stages = colors.value.length - 1;
+	
+		for (let index = 0; index < stages; index++) {
+			const textToSend = nick.value.slice(
+				Math.floor(sections * index),
+				Math.floor(sections * (index + 1))
+			);
+	
+			out += useCalculateGradientColors(
+				textToSend,
+				colors.value[index],
+				colors.value[index + 1]
+			);
+		}
+	
+		nick.value = out;
 	}
-
-	let out = "";
-	nick.value = nick.value.replace(/&([0-9a-f]|#[0-9a-f]{6})/gi, "");
-
-	const sections = nick.value.length / (colors.value.length - 1);
-	const stages = colors.value.length - 1;
-
-	for (let index = 0; index < stages; index++) {
-		const textToSend = nick.value.slice(
-			Math.floor(sections * index),
-			Math.floor(sections * (index + 1))
-		);
-
-		out += useCalculateGradientColors(
-			textToSend,
-			colors.value[index],
-			colors.value[index + 1]
-		);
-	}
-
-	nick.value = out;
 }
