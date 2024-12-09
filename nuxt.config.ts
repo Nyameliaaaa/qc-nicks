7e58c2daab6bf3d92547c1ce223a69ab5fd424f2
Amelia Rose <nyameliaaaa@proton.me>
2024-12-10 00:41:28 +0300

    istfg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,18 +7,23 @@ export default defineNuxtConfig({
     "@sidebase/nuxt-auth",
     "@nuxthub/core",
   ],
+
   colorMode: {
     preference: "system",
     fallback: "light",
     classSuffix: "",
     storageKey: "color-mode",
   },
+
   css: ["@/assets/css/tailwind.css"],
   tailwindcss: { exposeConfig: true },
+
   imports: {
     dirs: ["composables/**"],
   },
+
   auth: {
+    baseURL: process.env.AUTH_ORIGIN,
     provider: {
       type: "authjs",
       trustHost: false,
@@ -26,12 +31,16 @@ export default defineNuxtConfig({
       addDefaultCallbackUrl: true,
     },
   },
+
   hub: {
     database: true,
   },
+
   runtimeConfig: {
     secret: process.env.NUXT_AUTH_SECRET ?? "",
     clientId: process.env.DISCORD_CLIENT_ID ?? "",
     clientSecret: process.env.DISCORD_CLIENT_SECRET ?? "",
   },
+
+  compatibilityDate: "2024-12-10",
 });
